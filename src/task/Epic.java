7e0e57df372f54f9574a85c9959636551e3671b4
Golubev Constantine <constantineg2008@yandex.ruecho>
2023-12-08 14:53:47 +0300

    commit fix errors

--- src/task/Epic.java
+++ src/task/Epic.java
@@ -3,20 +3,17 @@ package task;
 import java.util.ArrayList;
 
 public class Epic extends Task {
+    private final ArrayList<Integer> subtaskIds;
 
-    public void setSubtaskIds(ArrayList<Integer> subtaskIds) {
-        this.subtaskIds = subtaskIds;
-    }
-
-    private ArrayList<Integer> subtaskIds;
-
-    public Epic( String name, String description) {
-        super( name, description);
+    public Epic(String name, String description) {
+        super(name, description);
         this.subtaskIds = new ArrayList<>();
     }
-    public void addSubtaskId(int subtaskId){
+
+    public void addSubtaskId(int subtaskId) {
         subtaskIds.add(subtaskId);
     }
+
     public ArrayList<Integer> getSubtaskIds() {
         return subtaskIds;
     }
--- src/task/Subtask.java
+++ src/task/Subtask.java
@@ -1,15 +1,15 @@
 package task;
 
-public class Subtask extends Task{
+public class Subtask extends Task {
 
     private final int epicId;
 
-    public Integer getEpicId() {
-        return epicId;
-    }
-
-    public Subtask( String name, String description, Integer epicId) {
+    public Subtask(String name, String description, Integer epicId) {
         super(name, description);
         this.epicId = epicId;
+    }
+
+    public Integer getEpicId() {
+        return epicId;
     }
 }
