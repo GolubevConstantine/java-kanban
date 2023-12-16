626c0926c54c49956a85ee3c7480c26eba9c8c40
Golubev Constantine <constantineg2008@yandex.ruecho>
2023-12-16 12:48:03 +0300

    fix reviewer comments

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -2,34 +2,28 @@ package manager;
 
 import task.Task;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final List<Task> ids;
-
-    public InMemoryHistoryManager() {
-        this.ids = new ArrayList<>();
-    }
+    private final LinkedList<Task> history = new LinkedList<>();
 
     @Override
     public void add(Task task) {
-        if (task==null) {
+        if (task == null) {
             return;
         }
 
-        if (ids.size() < 10) {
-            ids.add(task);
-        } else {
-            ids.remove(0);
-            ids.add(task);
+        if (history.size() >= 10) {
+            history.removeFirst();
         }
+        history.add(task);
     }
 
     @Override
     public List<Task> getHistory() {
-        return Collections.unmodifiableList(ids);
+        return Collections.unmodifiableList(history);
     }
 }
