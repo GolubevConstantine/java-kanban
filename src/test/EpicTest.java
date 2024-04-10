5decc0f6fb83bcf2e71706a67c13c3b7a5224647
Golubev Constantine <constantineg2008@yandex.ruecho>
2024-04-10 15:23:46 +0300

    fix checkstyle

--- src/test/EpicTest.java
+++ src/test/EpicTest.java
@@ -9,11 +9,11 @@ import task.Subtask;
 
 class EpicTest {
     @Test
-    void addEpicInSubtask(){
+    void addEpicInSubtask() {
         TaskManager inMemoryTaskManager = Managers.getDefault();
         Epic epic = new Epic("Epic #1", "Description");
         inMemoryTaskManager.addEpic(epic);
-        Subtask subtask = new Subtask("New subtask", "Description",2);
+        Subtask subtask = new Subtask("New subtask", "Description", 2);
         inMemoryTaskManager.addSubtask(subtask);
     }
 }
